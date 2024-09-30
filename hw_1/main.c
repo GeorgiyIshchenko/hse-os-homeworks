a3bef116a7a323662621fca9e5b74961f23a9ec2
Georgiy Ishchenko <gogaigd2005@gmail.com>
2024-10-01 01:34:11 +0300

    fix(hw_1)

--- hw_1/main.c
+++ hw_1/main.c
@@ -6,6 +6,7 @@
 int main()
 {
   int     fd[2], result;
+  size_t  size;
 
   if (pipe(fd) < 0) {
     perror("Can\'t open pipe\n");
@@ -25,10 +26,25 @@ int main()
       perror("child: Can\'t close writing side of pipe\n"); exit(-1);
     }
 
-    int exitCode = execl("reader.out", "reader.out", fd[0], NULL);
+    char resstring[13];
+
+    size = read(fd[0], resstring, 13);
+
+    if (size < 0) {
+      perror("Can\'t read string from pipe\n");
+      exit(-1);
+    }
+
+    if (close(fd[0]) < 0) {
+      perror("parent: Can\'t close reading side of pipe\n"); exit(-1);
+    }
+
+    printf("Parent read, string: %s\n", resstring);
+
+    int exitCode = execl("/bin/echo", "echo", resstring, NULL);
 
     if (exitCode) {
-      perror("Parent exec reader.out with error!\n");
+      perror("Parent exec cat with error!\n");
     }
 
   } else {
@@ -39,10 +55,16 @@ int main()
       perror("parent: Can\'t close reading side of pipe\n"); exit(-1);
     }
 
-    int exitCode = execl("writer.out", "writer.out", fd[1], NULL);
+    if (dup2(fd[1], STDOUT_FILENO) == -1) {
+      perror("Dup2 stdout error!\n");
+      close(fd[1]);
+      exit(-1);
+    }
+
+    int exitCode = execl("/bin/echo", "echo", "SOME_MESSAGE", NULL);
 
     if (exitCode) {
-      perror("Child exec writer.out with error!\n");
+      perror("Child exec echo with error!\n");
     }
 
   }
--- run_all.py
+++ run_all.py
@@ -9,7 +9,9 @@ RUN_FILE_NAME = "run.sh"
 
 for name in os.listdir("."):
     if HW_DIR_RE.search(name) != None:
+        print(f"RUNNING HOMEWORK: {name}")
         os.chdir(name)
         run_path = os.path.join(os.path.abspath(
             os.path.dirname(name)), RUN_FILE_NAME)
         subprocess.run(run_path, shell=True)
+        print()
